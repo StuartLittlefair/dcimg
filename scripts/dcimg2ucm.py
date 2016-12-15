472c423e4420b4554323c73d66631406a8318f07
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-12-15 17:07:53 +0000

    PEP8 fixes

--- scripts/dcimg2ucm.py
+++ scripts/dcimg2ucm.py
@@ -4,10 +4,13 @@ from itertools import count
 from trm.ultracam import MCCD
 
 parser = argparse.ArgumentParser(description='Convert DCIMG file to many ucm files')
-parser.add_argument('file',help='dcimg file to convert')
-parser.add_argument('--start','-s',action='store',type=int,default=1,help='Start frame to grab')
-parser.add_argument('--end','-e',action='store',type=int,default=0,help='Start frame to grab (0 to grab all frames)')
-parser.add_argument('--ndigit','-n',action='store',type=int,default=3,help='number of digits in file numbers')
+parser.add_argument('file', help='dcimg file to convert')
+parser.add_argument('--start', '-s', action='store', type=int, default=1,
+                    help='Start frame to grab')
+parser.add_argument('--end', '-e', action='store', type=int, default=0,
+                    help='Start frame to grab (0 to grab all frames)')
+parser.add_argument('--ndigit', '-n', action='store', type=int, default=3,
+                    help='number of digits in file numbers')
 args = parser.parse_args()
 
 rdat = dcimg.Ddata(args.file)
@@ -16,9 +19,10 @@ if args.end == 0:
 
 rdat.set(args.start)
 
-filename_gen = ('{name}_{num:0{width}d}.ucm'.format(name=args.file,num=i,width=args.ndigit) for i in count(1))
+filename_gen = ('{name}_{num:0{width}d}.ucm'.format(name=args.file, num=i,
+                                                    width=args.ndigit) for i in count(1))
 while rdat.nframe() < args.end:
     filename = next(filename_gen)
     ccd = rdat()
-    mccd = MCCD([ccd],ccd.head)
+    mccd = MCCD([ccd], ccd.head)
     mccd.wucm(filename)
